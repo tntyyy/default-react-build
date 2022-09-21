28d9d326fe32fc6086e755dee54b151d58403b37
tnty <tnty.working@gmail.com>
2022-09-21 19:39:22 +0300

    base

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { sum } from "./test";
+import './styles/index.css';
+
+console.log(sum(Math.random(), Math.random()));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./index.js"
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+}
